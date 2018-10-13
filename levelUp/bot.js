065d3c7057f964da4828ea85364b10af46826700
get-thepacket <lakshyab.1999@gmail.com>
2018-10-13 20:25:48 +0530

    bot.js updated
    line 1,2 update require

--- levelUp/bot.js
+++ levelUp/bot.js
@@ -1,5 +1,5 @@
-var Discord = require('./node_modules/discord.io');
-var logger = require('./node_modules/winston');
+var Discord = require('discord.io');
+var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
