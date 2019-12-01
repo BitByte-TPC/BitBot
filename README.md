0a925ce5c4e8a8d39a1a8b45bc52e7b681e3b19a
get-thepacket <lakshyab.1999@gmail.com>
2019-12-02 00:14:23 +0530

    readme changes and dailymeme added

--- README.md
+++ README.md
@@ -10,6 +10,16 @@ _ToDo_ :-
 - [X] Scrap working independent but not with bot (solved, link problem)
 - [X] Roles add and remove added (ezpz!)
 - [X] Set roles for codechef coders (if guild has it, Competitive Coder will be assigned)
+- [X] xkcd image fetch added (as dailymeme) 
 - [ ] add some todos (plz!)
+
+---
+
+# Features till now :-
+- Can ping someone
+- Can produce a help page
+- Can remove/add roles from member
+- Can fetch xkcd memes
+- Can give info about codechef user by supplying username
 
 ---
---
+++ commands/dailymeme.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+exports.run = (client,message,args) =>{
+    let rand = (Math.round(Math.random()*10000))%2236;
+    if(rand==0){
+        message.channel.send("Unlucky day, Sorry!");
+        return;
+    }
+    let options = {
+        uri : `https://xkcd.com/${rand}`,
+        headers : {
+            'User-Agent' : 'Mozilla/5.0'
+        }
+    }
+    rp(options).then(html => {
+        const $ = cheerio.load(html);
+        let link = $("#comic img").attr('src');
+        link = "https:"+link;
+        message.channel.send("Your educational meme ==",{files:[link]});
+    }).catch(err => console.error);
+}
+
+exports.info = "Fetch a random image from internet, if you are unlucky then return a message";
