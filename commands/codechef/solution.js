23f8cb4d4bc58638fccced2173c012481e3f21f3
get-thepacket <lakshyab.1999@gmail.com>
2019-12-03 19:14:12 +0530

    bug fixes and removed links from solution.

--- commands/codechef/solution.js
+++ commands/codechef/solution.js
@@ -3,18 +3,19 @@ const Discord = require('discord.js');
 // const hljs = require('highlight.js');
 
 exports.run = (client,message,args) => {
-    //gives out the info of the solution - it's link, profile link and contest link
-    web.getUser(args, user => {
-        if(!user){
-            message.channel.send("User doesn't exists");
-            return;
-        }
-        let embed = new Discord.RichEmbed();
-        embed.addField("Problem link",user.problem)
-            //.addField("Contest link",user.contest)
-            .addField("Profile link",user.profile);
-        message.channel.send(embed);
-    });
+    // //in testing
+    // //gives out the info of the solution - it's link, profile link and contest link
+    // web.getUser(args, user => {
+    //     if(!user){
+    //         message.channel.send("User doesn't exists");
+    //         return;
+    //     }
+    //     let embed = new Discord.RichEmbed();
+    //     embed.addField("Problem link",user.problem)
+    //         //.addField("Contest link",user.contest)
+    //         .addField("Profile link",user.profile);
+    //     message.channel.send(embed);
+    // });
     //calls getSub function to get solution as plaintext and format it here
     web.getSub(args,text => {
         if(!text){
@@ -23,8 +24,12 @@ exports.run = (client,message,args) => {
         }    
         // let embed = new Discord.RichEmbed();
         // text = hljs.highlightAuto(text).value;
-        text = "```"+text+"```"; //add backticks for formatting
-        message.channel.send(`${message.author.username}'s Solution :`+text);
+        //text = "```"+text+"```"; //add backticks for formatting
+        text = text.match(/(.|[\r\n]){1,1990}/g); //found on stackoverflow split string in list with element size 1990
+        message.channel.send(`${message.author.username}'s Solution :`);
+        text.forEach(e => {
+            message.channel.send("```"+e+"```");
+        });
     });
     
 }
