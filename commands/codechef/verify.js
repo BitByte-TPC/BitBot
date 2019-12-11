ed6999605764018dda2652f49717f46b4eb2eb16
get-thepacket <lakshyab.1999@gmail.com>
2019-12-11 14:58:00 +0530

    role added to verify

--- commands/codechef/verify.js
+++ commands/codechef/verify.js
@@ -4,6 +4,9 @@ const path = require('path');
 const base = require(path.join(__dirname,"../database/db_json.js"));
 exports.run = async function (client,message,args){
     // const ver = await web.verify(args);
+    if(base.getUserByCCHandle(args[0]))
+        message.channel.send("You have been already verified for Codechef!");
+    else{    
     const question = await web.randomQuestion(); //get's link of the question [.link] and code[.pcode]
     let embed = new Discord.RichEmbed();
     embed.addField("You have 1 min, submit a compilation error to this question",question.link);
@@ -12,20 +15,47 @@ exports.run = async function (client,message,args){
     // let newTime = Date.now()/1000;
     setTimeout(async function(){
         let _data = await web.verify(args);
+        let data = {
+            username : message.member.user.tag,
+            codechef_handle : args[0],
+            codeforces_handle : ""
+        };
         if(_data.pcode == question.pcode && (_data.date =='1' || _data.date == '2') && _data.status == "compilation error"){
-            let data = {
-                username : message.member.user.tag,
-                codechef_handle : args[0],
-                codeforces_handle : ""
-            };
+            
             if(!base.addUser(data))
                 base.updateCCHandle(data.username,args[0]);
-            message.channel.send(`Congratulations, you are now verified!`);
+            message.channel.send(`Congratulations, ${data.username.split("#")[0]} is now verified!\nYour role will be added soon.`);
+            web.getData(args, user => {
+                let roleString = "codechef " +user.stars
+                let roles = message.member.roles; //list of role member have
+                let role = message.guild.roles.find(r => r.name === roleString); //role that will be added
+
+                if(roles.find(`name`,roleString)){
+                    return;
+                }
+                //removing other star roles
+                for(var i = 1 ; i < 8 ; i++ ){
+                    let check = roles.find(r => r.name === `codechef ${i}★`);
+                    // let check = roles.find('name',`codechef ${i}★`);
+                    if(check){
+                        message.member.removeRole(check);
+                    }
+                }
+
+                if(role){
+                    message.member.addRole(role);
+                }
+                else {
+                    message.channel.send(`Guild doesn't have ${user.rank} role`);
+                }
+            });
         }
         else{
-            message.channel.send("It's OK, try again. You are not verified!");
+            message.channel.send(`It's OK, try again. ${data.username} is not verified!`);
         }
-    },1.5*60*1000)
-
+    },1.5*60*1000);
+}
 
 };
+
+exports.info = "Verification for codechef and role add"
--- commands/codeforces/verify.js
+++ commands/codeforces/verify.js
@@ -6,6 +6,9 @@ const base = require(path.join(__dirname,"../database/db_json.js"));
 exports.run = (client,message,args) => {
     // var timeWhenQuestionIsPut;
     // let data;
+    if(base.getUserByCFHandle(args[0]))
+        message.channel.send("You have been already verified for Codeforces!");
+    else{
     let gdata = {}
     web.getRandomQuestion(json => {
     let embed = new Discord.RichEmbed();
@@ -15,26 +18,46 @@ exports.run = (client,message,args) => {
     message.channel.send(embed);
     gdata.timeWhenQuestionIsPut = Date.now()/1000;
     });
-
     setTimeout(()=> {
         web.getStatus(args, status=> {
             // console.log(args);
+            let data = {
+                username : message.member.user.tag,
+                codechef_handle : "",
+                codeforces_handle : args[0]
+            };
             if(status.cid == gdata.data.contestId && status.index == gdata.data.index && status.time - gdata.timeWhenQuestionIsPut <= 60 && status.verdict == "COMPILATION_ERROR"){
-                let data = {
-                    username : message.member.user.tag,
-                    codechef_handle : "",
-                    codeforces_handle : args[0]
-                };
+                
 
-                let ans = base.addUser(data);
-                if(!ans)
+                if(!base.addUser(data))
                     base.updateCFHandle(data.username,args[0]);
-                message.channel.send(`Congratulations, you are now verified!`);     
+                message.channel.send(`Congratulations, ${data.username.split("#")[0]} is now verified!\nYour role will be added soon!`);
+                web.getDataOfUser(args, user => {
+                    // let role = message.guild.roles.find(r => r.name === user.rank);
+                    let ranks = ["pupil","specialist","expert","candidate master","master","international master","grandmaster","international grandmaster","legendary grandmaster"];
+                    let roles = message.member.roles; //list of role member have
+                    // let role = message.guild.roles.find('name',user[0].rank); 
+                    let role = message.guild.roles.find(r=> r.name == user[0].rank);
+                    ranks.forEach(e => {
+                        let check = roles.find( r=> r.name == e);
+                        // let check = roles.find('name',e);
+                        if(check)
+                            message.member.removeRole(check);
+                    });
+                    if(!role)
+                        message.channel.send(`Guild doesn't have ${user[0].rank} role`);
+                    else {
+                        message.member.addRole(role);
+                    }
+                    
+                });
             }
             else{
-                message.channel.send(`It's OK, try again. You are not verified!`);
+                message.channel.send(`It's OK, try again. ${data.username} is not verified!`);
             }
         })
     },60*1000);
-    
+}    
 };
+
+exports.info = "Verification for codeforces and role add"
