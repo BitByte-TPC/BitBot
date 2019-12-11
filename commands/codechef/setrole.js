ee2421311d50c96590ea478af276449576475a49
Meru Patel <2017180@iiitdmj.ac.in>
2019-12-11 15:09:13 +0000

    callbacks replaced

--- commands/codechef/setrole.js
+++ commands/codechef/setrole.js
@@ -1,61 +1,63 @@
 const Discord = require('discord.js');
-const web = require('./scrape/scrap');
+const cc = require('./scrape/scrap');
 const path = require('path');
-const base = require(path.join(__dirname,"../database/db_json.js"));
-exports.run = async function (client,message,args){
-    // const ver = await web.verify(args);
-    if(base.getUserByCCHandle(args[0]))
-        message.channel.send("You have been already verified for Codechef!");
-    else{    
-    const question = await web.randomQuestion(); //get's link of the question [.link] and code[.pcode]
-    let embed = new Discord.RichEmbed();
-    embed.addField("You have 1 min, submit a compilation error to this question",question.link);
-    message.channel.send(embed);
+const base = require(path.join(__dirname, "../database/db_json.js"));
+
+exports.run = async function (client, message, args) {
     
-    // let newTime = Date.now()/1000;
-    setTimeout(async function(){
-        let _data = await web.verify(args);
+    let handle = args[0];
+
+    if (base.getUserByCCHandle(handle)) {
+        message.channel.send("Your rank will be updated");
+        setRole(message.member, handle);
+        return;
+    }
+
+    const question = await cc.randomQuestion(); 
+    let embed = new Discord.RichEmbed();
+    embed.addField("You have 1 min, submit a compilation error to this question", question.link);
+    message.channel.send(embed);
+
+    setTimeout(async function () {
+        let submission = await cc.getLastSubmission(handle);
+
         let data = {
-            username : message.member.user.tag,
-            codechef_handle : args[0],
-            codeforces_handle : ""
+            username: message.member.user.tag,
+            codechef_handle: args[0],
+            codeforces_handle: ""
         };
-        if(_data.pcode == question.pcode && (_data.date =='1' || _data.date == '2') && _data.status == "compilation error"){
-            
-            if(!base.addUser(data))
-                base.updateCCHandle(data.username,args[0]);
-            message.channel.send(`Congratulations, ${data.username.split("#")[0]} is now verified!\nYour role will be added soon.`);
-            web.getData(args, user => {
-                let roleString = "codechef " +user.stars
-                let roles = message.member.roles; //list of role member have
-                let role = message.guild.roles.find(r => r.name === roleString); //role that will be added
 
-                if(roles.find(`name`,roleString)){
-                    return;
-                }
-                //removing other star roles
-                for(var i = 1 ; i < 8 ; i++ ){
-                    let check = roles.find(r => r.name === `codechef ${i}★`);
-                    // let check = roles.find('name',`codechef ${i}★`);
-                    if(check){
-                        message.member.removeRole(check);
-                    }
-                }
+        if (submission.pcode == question.pcode) {
+            if (!base.addUser(data)){
+                base.updateCCHandle(data.username, handle);
+            }
 
-                if(role){
-                    message.member.addRole(role);
-                }
-                else {
-                    message.channel.send(`Guild doesn't have ${user.rank} role`);
-                }
-            });
+            message.channel.send(`You are now verified, ${message.member}!\nYour role will be added soon.`);
+            setRole(message.member, handle);
         }
-        else{
-            message.channel.send(`It's OK, try again. ${data.username} is not verified!`);
+        else {
+            message.channel.send(`It's OK, try again. ${message.member} is not verified!`);
         }
-    },1.5*60*1000);
-}
-
+    }, 60 * 1000);
 };
+
+
+let setRole = async (member, id) => {
+    let user = await cc.getUserInfo(id);
+    let newRole = 'Codechef ' + user.stars;
+
+    let roles = member.roles;
+    oldRole = roles.find(r => r.name.startsWith('Codechef'))
+
+    if (oldRole) {
+        member.removeRole(oldRole);
+    }
+
+    let role = member.guild.roles.find(r => r.name == newRole);
+    if (!role) {
+        return;
+    }
+    member.addRole(role);
+}
 
 exports.info = "Verification for codechef and role add"
--- commands/codeforces/solution.js
+++ commands/codeforces/solution.js
@@ -2,29 +2,30 @@ const web = require('./api/calls.js');
 const Discord = require('discord.js');
 
 exports.run = (client,message,args) => {
-    //console.log(args);
-    web.getUser(args, user=>{
-        if(!user){
-            message.channel.send("User doesn't exists");
-            return;
-        }
-        let embed = new Discord.RichEmbed();
-        embed.addField("Profile link:",user.profile)
-            .addField("Problem link:",user.problem);
-        message.channel.send(embed);
-    });
-    web.getSub(args, text=>{
-        if(!text){
-            message.channel.send("Solution not found please enter again.");
-            return;
-        }
+    message.channel.send('Command is not added yet');
+    // //console.log(args);
+    // web.getUser(args, user=>{
+    //     if(!user){
+    //         message.channel.send("User doesn't exists");
+    //         return;
+    //     }
+    //     let embed = new Discord.RichEmbed();
+    //     embed.addField("Profile link:",user.profile)
+    //         .addField("Problem link:",user.problem);
+    //     message.channel.send(embed);
+    // });
+    // web.getSub(args, text=>{
+    //     if(!text){
+    //         message.channel.send("Solution not found please enter again.");
+    //         return;
+    //     }
 
-        text = text.match(/(.|[\r\n]){1,1990}/g); //found on stackoverflow split string in list with element size 1990
-        message.channel.send(`${message.author.username}'s Solution :`);
-        text.forEach(e => {
-            message.channel.send("```"+e+"```");
-        });
-    });
+    //     text = text.match(/(.|[\r\n]){1,1990}/g); //found on stackoverflow split string in list with element size 1990
+    //     message.channel.send(`${message.author.username}'s Solution :`);
+    //     text.forEach(e => {
+    //         message.channel.send("```"+e+"```");
+    //     });
+    // });
     
 };
 
