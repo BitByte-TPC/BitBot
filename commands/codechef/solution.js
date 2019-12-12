088985cc74ade8dba2964fd773b417f6f5d71da0
get-thepacket <lakshyab.1999@gmail.com>
2019-12-12 12:45:35 +0530

    cc solution color added

--- commands/codechef/solution.js
+++ commands/codechef/solution.js
@@ -3,38 +3,36 @@ const Discord = require('discord.js');
 // const hljs = require('highlight.js');
 
 exports.run = async function(client,message,args){
-    message.channel.send('Command is not added yet');
+    // message.channel.send('Command is not added yet');
 
-    // //in testing
-    // //gives out the info of the solution - it's link, profile link and contest link
-    // const p = await web.getSub(args);
-    // const profile = p.data;
-    // console.log(profile);
-    // if(profile.testInfo == ''){
-    //     message.channel.send("Solution not found!");
-    //     return;
-    // }
-    // let colors = {
-    //     "Compilation Error" : "ffcf2a",
-    //     "Accepted" : "12c200",
-    //     "Rejected" : "df0101",
-    //     "Time Limit Exceeded" : "df0101",
-    //     "Runtime Error":"df0101"
-    // }
-    // let text = profile.plaintext;
-    // text = text.match(/(.|[\r\n]){1,1990}/g); //found on stackoverflow split string in list with element size 1990
-    // let embed = new Discord.RichEmbed();
-    // const home = "https://www.codechef.com";
-    // embed.addField("Problem Link",home+profile.problemUrl)
-    //     .addField("Contest Link",home+profile.contestUrl)
-    //     .addField("Profile Link of Submitter",home+"/users/"+profile.solutionOwnerHandle)
-    //     .setColor(colors[profile.humanReadableResult]);
-    
-    // message.channel.send(embed);
-
-    // text.forEach(e => {
-    //     message.channel.send("```"+e+"```");
-    // });
+    //in testing
+    //gives out the info of the solution - it's link, profile link and contest link
+    const p = await web.getSub(args);
+    const profile = p.data;
+    if(profile.testInfo == ''){
+        message.channel.send("Solution not found!");
+        return;
+    }
+    let colors = {
+        "Compilation Error" : "ffcf2a",
+        "Accepted" : "12c200",
+        "Rejected" : "df0101",
+        "Time Limit Exceeded" : "df0101",
+        "Runtime Error":"df0101"
+    }
+    let text = profile.plaintext;
+    text = text.match(/(.|[\r\n]){1,1990}/g); //found on stackoverflow split string in list with element size 1990
+    let embed = new Discord.RichEmbed();
+    const home = "https://www.codechef.com";
+    embed.addField("Problem Link",home+profile.problemUrl)
+        .addField("Contest Link",home+profile.contestUrl)
+        .addField("Profile Link of Submitter",home+"/users/"+profile.solutionOwnerHandle)
+        .setColor(colors[profile.humanReadableResult]);
+    let lang = profile.languageExtension;
+    message.channel.send(embed);
+    text.forEach(e => {
+        message.channel.send("```"+lang+'\n'+e+"```");
+    });
     
 }
 
