321be57189e86deb375162dcc37b0404d835f1b4
Glitch (get-thepacket-discord-bot) <none>
2019-12-20 16:30:46 +0000

    cf user embed color added

--- commands/codeforces/user.js
+++ commands/codeforces/user.js
@@ -2,7 +2,28 @@ const Discord = require('discord.js');
 const cf = require('./api/calls.js');
 
 exports.run = async (client, message, args) => {
-
+    const color = {
+      "3000-9999" : "#FF0000",
+      "2600-2999" : "#FF00FF",
+      "2400-2599" : "#FF00FF",
+      "2100-2399" : "#FF8C00",
+      "1900-2099" : "#AA00AA",
+      "1600-1899" : "#0000FF",
+      "1400-1599" : "#03A89E",
+      "1200-1399" : "#88CC22",
+      "0-1199" : "#646464"
+    }  
+    
+    let colour = (value) => {
+      var item;
+      Object.keys(color).some(k => {
+        var part = k.split('-');
+        if(+value < part[1] && +value > part[0])
+          item = color[k];
+      });
+      return item;
+    }
+  
     let user = await cf.getUserInfo(args);
     user.forEach(newUser => {
       let embed = new Discord.RichEmbed();
@@ -13,6 +34,7 @@ exports.run = async (client, message, args) => {
         .addField("Rating", newUser.rating, true)
         .addField("Highest Rank", newUser.maxRank, true)
         .addField("Highest Rating", newUser.maxRating, true)
+        .setColor(colour(newUser.rating))
         .setThumbnail("https:"+newUser.avatar);
     message.channel.send(embed);  
     });
