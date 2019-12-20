6072dfb457d862e11ec8d49a0c42dba3d6a048b5
Glitch (get-thepacket-discord-bot) <none>
2019-12-20 12:15:30 +0000

    🎀🚂 Updated with Glitch

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-const Discord = require('discord.js');
-const config = require('./config.js').config;
-const Enmap = require('enmap');
-const fs = require('fs');
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+const app = express();
 
-const client  = new Discord.Client();
+const config = {
+    "token": process.env.TOKEN,
+    "prefix":"-",
+    "ownerID" : "get-thepacket"
+};
 
+const client = new Discord.Client();
 client.config = config;
 
-fs.readdir('./events/',(err,files)=>{
-    if(err) return console.error(err);
-    files.forEach(file =>{
-        const event = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        client.on(eventName,event.bind(null, client));
-    });
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
 });
 
 client.commands = new Enmap(); // make a map for command => command file path
 
-fs.readdir('./commands/',(err,files)=>{
-    if(err) return console.error(err);
-    files.forEach(file=>{
-        if(!file.endsWith('.js')) return;
-        let props = require(`./commands/${file}`);
-        let commandName = file.split(".")[0];
-        console.log(`attempting to load command ${commandName}`);
-        client.commands.set(commandName,props);
-    });
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
+
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 client.login(config.token);
